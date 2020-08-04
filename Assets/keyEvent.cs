938086fcfd788e3873f93a600f47ff9a17881260
GuanFei_1993 <1845008878@qq.com>
2020-08-04 16:25:34 +0800

    no message

---
+++ Assets/keyEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class keyEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UnityEvent UnityEvent;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("sssssss");
+            UnityEvent?.Invoke();
+        }
+    }
+}
